df612faab17680238c0516f2cf5771ae08391a52
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-04-29 23:53:12 +0200

    initial commit

---
+++ android/app/src/main/java/ch/receiptvault/app/MainActivity.java
@@ -0,0 +1,5 @@
+package ch.receiptvault.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+	appId: 'ch.receiptvault.app',
+	appName: 'Receipt Vault',
+	webDir: 'build'
+};
+
+export default config;
